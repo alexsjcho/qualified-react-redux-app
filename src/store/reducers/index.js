3e96f0e8d3f08155173070256e4de68ef181fe5a
Alex Cho <alexsjcho@gmail.com>
2019-04-17 17:18:00 -0700

    1st iteration of setQualifcation reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -17,6 +17,7 @@ settings = {
   authority: {
     questions: [
       {
+        // All questions have personas
         question: "who makes the buying decision?",
         type: "text input",
         personaComments: "",
@@ -29,27 +30,61 @@ settings = {
       { value: "executive", metric: 4 },
       { value: "CXO", metric: 5 }
     ],
+
+    roles: [
+      { value: "influencer", metric: 2 },
+      { value: "champion", metric: 3 },
+      { value: "decision-maker", metric: 4 }
+    ],
     comment: ""
   },
 
   need: {
-    question: [""],
-    departments: {
-      type: [
-        "sales",
-        "marketing",
-        "leadership",
-        "product",
-        "engineering",
-        "human resource"
-      ],
-      departmentComments: ""
-    },
+    questions: [
+      {
+        // All questions have departments and personas
+        question: "how much money are you losing per month?",
+        type: "select",
+        options: [
+          { value: "less than $500", metric: 5 },
+          { value: "$500 - $2000", metric: 10 },
+          { value: "more than $2000", metric: 20 }
+        ],
+        comment: "",
+        required: true | false
+      }
+    ],
+    departments: [
+      { value: "marketing", metric: 2 },
+      { value: "sales", metric: 3 },
+      { value: "product", metric: 4 },
+      { value: "leadership", metric: 5 }
+    ],
     comment: ""
   },
   timeline: {
-    closeDate: "",
-    comment: ""
+    questions: [
+      {
+        question: "when do you want this change to happen?",
+        closeDate: "",
+        comment: "",
+        required: true | false
+      }
+    ]
+  },
+
+  objections: {
+    questions: [
+      {
+        question: "your product is too expensive",
+        comment: "",
+        impact: [
+          { value: "big", metric: -5 },
+          { value: "medium", metric: -4 },
+          { value: "small", metric: -3 }
+        ]
+      }
+    ]
   },
 
   nextMeeting: {
