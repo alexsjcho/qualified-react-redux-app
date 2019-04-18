6b0e9c987b4be7a27ef803f2cc5e39b69d515e3c
Alex Cho <alexsjcho@gmail.com>
2019-04-17 18:10:15 -0700

    2nd iteration of settingsQualification reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,99 +1,125 @@
 // 1st iteration
 settings = {
   qualification: {
-    budget: {
-      comment: "",
-      questions: [
-        {
-          question: "Does this company has $15,000 budget?",
-          type: "radio | select",
-          options: [{ value: "yes", metric: 20 }, { value: "no", metric: 0 }],
-          required: true | false
-        }
-      ]
+    sections: {
+      budget: {
+        label: "Budget",
+        comment: "",
+        questions: [
+          {
+            label: "Configure budget target",
+            question: "Does this company has $15,000 budget?",
+            type: "radio | select",
+            options: [{ value: "yes", metric: 20 }, { value: "no", metric: 0 }],
+            required: true | false
+          }
+        ]
+      },
+      authority: {
+        label: "Authority",
+        questions: [
+          {
+            // All questions have personas
+            label: "Set ideal customer personal?",
+            question: "who makes the buying decision?",
+            type: "text input",
+            required: true | false,
+            hasPersonas: true | false
+          }
+        ],
+        comment: ""
+      },
+
+      need: {
+        label: "Need",
+        questions: [
+          {
+            // All questions have departments and personas
+            label: "Set pain point criterias",
+            question: "how much money are you losing per month?",
+            type: "select",
+            options: [
+              { value: "less than $500", metric: 5 },
+              { value: "$500 - $2000", metric: 10 },
+              { value: "more than $2000", metric: 20 }
+            ],
+            comment: "",
+            required: true | false,
+            hasPersonas: true | false
+          }
+        ],
+        comment: ""
+      },
+      timeline: {
+        label: "Timeline",
+        questions: [
+          {
+            label: "Set deal close timeline",
+            question: "when do you want this change to happen?",
+            type: "date-input",
+            required: true | false
+          }
+        ],
+        comment: ""
+      },
+
+      objections: {
+        label: "Objections",
+        questions: [
+          {
+            label: "What is the severity of concern?",
+            question: "What concerns the client has?",
+            comment: "",
+            type: "select",
+            options: [
+              { value: "big", metric: -5 },
+              { value: "medium", metric: -4 },
+              { value: "small", metric: -3 }
+            ]
+          }
+        ]
+      },
+
+      nextMeeting: {
+        label: "Next Meeting",
+        questions: [
+          {
+            label: "Set next meeting",
+            question: "When is your next meeting?",
+            type: "date input",
+            required: true | false
+          }
+        ],
+        comment: "requirements"
+      }
+    },
+
+    score: {
+      totalScore: ""
     }
   },
-
-  authority: {
-    questions: [
-      {
-        // All questions have personas
-        question: "who makes the buying decision?",
-        type: "text input",
-        personaComments: "",
-        required: true | false
-      }
-    ],
-    personas: [
-      { value: "individual contributor", metric: 2 },
-      { value: "manager", metric: 3 },
-      { value: "executive", metric: 4 },
-      { value: "CXO", metric: 5 }
-    ],
-
-    roles: [
-      { value: "influencer", metric: 2 },
-      { value: "champion", metric: 3 },
-      { value: "decision-maker", metric: 4 }
-    ],
-    comment: ""
-  },
-
-  need: {
-    questions: [
-      {
-        // All questions have departments and personas
-        question: "how much money are you losing per month?",
-        type: "select",
-        options: [
-          { value: "less than $500", metric: 5 },
-          { value: "$500 - $2000", metric: 10 },
-          { value: "more than $2000", metric: 20 }
-        ],
-        comment: "",
-        required: true | false
-      }
-    ],
-    departments: [
-      { value: "marketing", metric: 2 },
-      { value: "sales", metric: 3 },
-      { value: "product", metric: 4 },
-      { value: "leadership", metric: 5 }
-    ],
-    comment: ""
-  },
-  timeline: {
-    questions: [
-      {
-        question: "when do you want this change to happen?",
-        closeDate: "",
-        comment: "",
-        required: true | false
-      }
-    ]
-  },
-
-  objections: {
-    questions: [
-      {
-        question: "your product is too expensive",
-        comment: "",
-        impact: [
-          { value: "big", metric: -5 },
-          { value: "medium", metric: -4 },
-          { value: "small", metric: -3 }
-        ]
-      }
-    ]
-  },
-
-  nextMeeting: {
-    date: "",
-    requirements: ""
-  },
-
-  score: {
-    currentScore: "",
-    totalScore: ""
-  }
+  discovery: {},
+  negotiation: {},
+  companies: [
+    //multipliers based off of the department value
+    {
+      departments: [
+        { value: "marketing", metric: 2 },
+        { value: "sales", metric: 3 },
+        { value: "product", metric: 4 },
+        { value: "leadership", metric: 5 }
+      ],
+      personas: [
+        { value: "individual", metric: 2 },
+        { value: "manager", metric: 3 },
+        { value: "executive", metric: 4 },
+        { value: "CXO", metric: 5 }
+      ],
+      roles: [
+        { value: "influencer", metric: 2 },
+        { value: "champion", metric: 3 },
+        { value: "decision-maker", metric: 4 }
+      ]
+    }
+  ]
 };
