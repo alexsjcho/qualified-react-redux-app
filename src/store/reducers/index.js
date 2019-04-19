1feaf736749bfc8c163c5997924e1294fdc8989b
Mario Retana <maretanar@gmail.com>
2019-04-19 15:58:19 -0600

    Using immutable combine reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,10 @@
-import opportunity from "./opportunity";
-import allOppDash from "./allOppDash";
-import settings from "./settings";
-import { combineReducers } from "redux";
+import opportunity from './opportunity'
+import allOppDash from './allOppDash'
+import settings from './settings'
+import { combineReducers } from 'redux-immutable'
 
 export default combineReducers({
   opportunity,
   allOppDash,
   settings
-});
+})
