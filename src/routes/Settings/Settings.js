3110af2e45f21155454cd8e2acaf80b4cf646ba1
Mario Retana <maretanar@gmail.com>
2019-04-20 16:50:44 -0600

    Added stage component.

--- src/routes/Settings/Settings.js
+++ src/routes/Settings/Settings.js
@@ -1,44 +1,11 @@
-import React from "react";
-import Section from "./components/Section";
-import { Container, Form } from "reactstrap";
+import React from 'react'
+import Stage from './components/Stage'
 
 export default class Settings extends React.PureComponent {
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      stage: props.stage
-    }
-  }
-  
-  handleSubmit = event => {
-    event.preventDefault();
-  };
-
-  updateSection = (sectionIndex, section) => {
-    this.setState(prevState => {
-      const nextStage = { ...prevState.stage };
-      nextStage.sections[sectionIndex] = section
-      return {
-        stage: nextStage
-      }
-    })
-  }
-
-  render() {
-    const { stage } = this.state;
-    const stageLabel = stage.stageLabel;
+  render () {
+    const { stage } = this.props
     return (
-      <Container>
-        <Form onSubmit={this.handleSubmit}>
-          <h1>{stageLabel}</h1>
-
-          {stage.sections
-            .map((section, index) => (
-              <Section section={section} sectionIndex={index} key={section.sectionId} updateSection={this.updateSection} />
-            ))}
-        </Form>
-      </Container>
-    );
+      <Stage stage={stage} />
+    )
   }
 }
---
+++ src/routes/Settings/components/Stage/Stage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Section from '../Section'
+import { Container, Form } from "reactstrap";
+
+export default class Stage extends React.PureComponent {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      stage: props.stage
+    }
+  }
+  
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  updateSection = (sectionIndex, section) => {
+    this.setState(prevState => {
+      const nextStage = { ...prevState.stage };
+      nextStage.sections[sectionIndex] = section
+      return {
+        stage: nextStage
+      }
+    })
+  }
+
+  render() {
+    const { stage } = this.state;
+    const stageLabel = stage.stageLabel;
+    return (
+      <Container>
+        <Form onSubmit={this.handleSubmit}>
+          <h1>{stageLabel}</h1>
+
+          {stage.sections
+            .map((section, index) => (
+              <Section section={section} sectionIndex={index} key={section.sectionId} updateSection={this.updateSection} />
+            ))}
+        </Form>
+      </Container>
+    );
+  }
+}
