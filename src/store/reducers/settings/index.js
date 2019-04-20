3fc99829b3b7f93ec92461d42b40a72b3e8a818e
Mario Retana <maretanar@gmail.com>
2019-04-20 17:57:16 -0600

    Added get settings from local storage.

--- src/store/reducers/settings/index.js
+++ src/store/reducers/settings/index.js
@@ -1,11 +1,12 @@
-import settings from "./settings.json";
-import { fromJS } from "immutable";
-import reducer from "./reducer";
+import settings from './settings.json'
+import { fromJS } from 'immutable'
+import reducer from './reducer'
+import { getSettings } from '../../../shared/utils'
 
-const initialState = fromJS(settings);
+const initialState = fromJS(getSettings() || settings)
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   return reducer[action.type]
     ? reducer[action.type](state, action.payload)
-    : state;
+    : state
 }
