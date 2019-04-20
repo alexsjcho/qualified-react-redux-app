495585d668e53606aa3465434c696ee06742b792
Mario Retana <maretanar@gmail.com>
2019-04-20 17:16:30 -0600

    Added action/reducer for submit stage

--- src/store/action/settings.js
+++ src/store/action/settings.js
@@ -1,35 +1,15 @@
-//Create const actions, types
+// Create const actions, types
 
 export const ACTION_TYPES = {
-    SET_LABEL_TEXT = " SET_LABEL_TEXT",
-    SET_QUESTION_TEXT = "SET_QUESTION_TEXT",
-    SET_PERSONAS = "SET_PERSONAS",
-    SET_COMMENT = " SET_COMMENT"
+  SUBMIT_STAGE: 'SUBMIT_STAGE'
+}
 
+export function submitStage (stage) {
+  return {
+    type: ACTION_TYPES.SUBMIT_STAGE,
+    payload: {
+      stage
+    }
   }
-
-
-export function setLabelText(params) {
-    return {
-        type: ACTION_TYPES.SET_LABEL_TEXT
-    }
 }
 
-
-export function setQuestionText(params) {
-    return {
-        type: ACTION_TYPES.SET_QUESTION_TEXT
-    }
-}
-
-export function setPersonas(params) {
-    return {
-        type: ACTION_TYPES.SET_PERSONAS
-    }
-}
-
-export function setComment(params) {
-    return {
-        type: ACTION_TYPES.SET_COMMENT
-    }
-}
--- src/store/reducers/settings/reducer.js
+++ src/store/reducers/settings/reducer.js
@@ -1,5 +1,11 @@
-let reducer = {};
+import { fromJS } from 'immutable'
+import ACTION_TYPES from '../../action/settings'
 
-// TODO: add action handlers
+let reducer = {}
 
-export default reducer;
+reducer[ACTION_TYPES.SUBMIT_STAGE] = (state, { stage }) => {
+  const stageIndex = state.get('stages').findIndex(currStage => currStage.get('stageId') === stage.stageId)
+  return state.setIn(['stages', stageIndex], fromJS(stage))
+}
+
+export default reducer
