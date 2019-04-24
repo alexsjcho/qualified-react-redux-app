f8ba9e477bcda22a5135f9fc7dc81890b6dde07f
Alex Cho <alexsjcho@gmail.com>
2019-04-23 18:33:28 -0700

    Fixed selector bug

--- src/shared/selectors.js
+++ src/shared/selectors.js
@@ -1,7 +1,8 @@
 import { createSelector } from "reselect";
 
 const getSettingStages = state => state.get("settings").get("stages");
-const getStageId = (state, props) => props.match.params.stageId;
+const getStageId = (state, props) =>
+  props.stageId || (props.match && props.match.params.stageId);
 
 export const getSettings = state => state.get("settings");
 
@@ -11,15 +12,15 @@ export const getCreatedOpportunityId = state =>
 export const getOpportunitiesList = state =>
   state.get("allOppDash").get("opportunitiesList");
 
-export const getCurrentStage = createSelector(
+export const getCurrentStageSettings = createSelector(
   [getSettingStages, getStageId],
   (stages, stageId) => {
     return stages.find(stage => stage.get("stageId") === stageId);
   }
 );
 
-export const getTotalStageScore = createSelector(
-  [getCurrentStage],
+export const getMaxPossibleStageScore = createSelector(
+  [getCurrentStageSettings],
   stage => {
     return stage.get("sections").reduce((accumulator, section) => {
       return (
