6cf2b8d9e31f89575fb2f7f6bd59fe3057c00529
Mario Retana <maretanar@gmail.com>
2019-04-25 15:48:56 -0600

    Added isGoal and isPainPoint to settings

--- src/routes/Settings/components/Section/Section.js
+++ src/routes/Settings/components/Section/Section.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FormGroup } from "reactstrap";
+import { FormGroup, Input, Label } from "reactstrap";
 import Question from "../Question";
 
 export default class Section extends React.PureComponent {
@@ -10,12 +10,46 @@ export default class Section extends React.PureComponent {
     updateSection(sectionIndex, newSection);
   };
 
+  setIsGoal = event => {
+    const { updateSection, sectionIndex, section } = this.props
+    const newSection = { ...section }
+    newSection.isGoal = !section.isGoal
+    updateSection(sectionIndex, newSection)
+  };
+
+  setIsPainPoint = event => {
+    const { updateSection, sectionIndex, section } = this.props
+    const newSection = { ...section }
+    newSection.isPainPoint = !section.isPainPoint
+    updateSection(sectionIndex, newSection)
+  };
+
   render() {
     const { section } = this.props;
 
     return (
       <FormGroup>
         <h2>{section.label}</h2>
+        <div>
+          <Input
+            onChange={this.setIsGoal}
+            type="checkbox"
+            name={`${section.sectionId}-is-goal`}
+            id={`${section.sectionId}-is-goal`}
+            checked={section.isGoal}
+          />
+          <Label for={`${section.sectionId}-is-goal`}>Does this section refer to goals?</Label>
+        </div>
+        <div>
+          <Input
+            onChange={this.setIsPainPoint}
+            type="checkbox"
+            name={`${section.sectionId}-is-pain-point`}
+            id={`${section.sectionId}-is-pain-point`}
+            checked={section.isPainPoint}
+          />
+          <Label for={`${section.sectionId}-is-pain-point`}>Does this section refer to pain points?</Label>
+        </div>
         {section.questions.map((question, index) => {
           return (
             <Question
