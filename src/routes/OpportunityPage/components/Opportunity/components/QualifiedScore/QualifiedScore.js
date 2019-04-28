d46656dbb36f01f61c2f458e2616a2f0f53b4696
Alex Cho <alexsjcho@gmail.com>
2019-04-27 17:17:59 -0700

    added progresBarColor logic ot QualifiedScore component

--- src/routes/OpportunityPage/components/Opportunity/components/QualifiedScore/QualifiedScore.js
+++ src/routes/OpportunityPage/components/Opportunity/components/QualifiedScore/QualifiedScore.js
@@ -2,6 +2,18 @@ import React from "react";
 import { Progress, Badge } from "reactstrap";
 
 export default class QualifiedScore extends React.PureComponent {
+  progressBarColor(stage, maxPossibleStageScore) {
+    let score = stage.score;
+    let maxScore = maxPossibleStageScore;
+    let percent = Math.round((score / maxScore) * 100);
+
+    if (percent <= 0.1) {
+      return "danger";
+    } else if (percent <= 0.5) {
+      return "primary";
+    }
+  }
+
   render() {
     const { stageSettings, maxPossibleStageScore, stage = {} } = this.props;
 
@@ -11,7 +23,7 @@ export default class QualifiedScore extends React.PureComponent {
           <Badge color="primary">{stageSettings.stageLabel} Stage Score</Badge>
         </h3>
         <Progress
-          color="info"
+          color={this.progressBarColor(stage, maxPossibleStageScore)}
           value={stage.score || 0}
           max={maxPossibleStageScore}>
           {stage.score || 0} of {maxPossibleStageScore}
