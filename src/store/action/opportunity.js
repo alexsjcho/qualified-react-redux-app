46021c8351fbba46410d44a7e9babd9cacb5e032
Mario Retana <maretanar@gmail.com>
2019-04-28 23:00:13 -0600

    Create an action to set or unset an objection as resolved

--- src/store/action/opportunity.js
+++ src/store/action/opportunity.js
@@ -1,9 +1,10 @@
-import { loadOpportunity, saveStage } from '../../shared/utils'
+import { loadOpportunity, saveStage, writeObjectionResolvedToggle } from '../../shared/utils'
 
 export const ACTION_TYPES = {
   SET_OPPORTUNITY: 'SET_OPPORTUNITY',
   SET_OPPORTUNITY_STAGE: 'SET_OPPORTUNITY_STAGE',
-  SET_OPPORTUNITY_STAGE_IS_SAVING: 'SET_OPPORTUNITY_STAGE_IS_SAVING'
+  SET_OPPORTUNITY_STAGE_IS_SAVING: 'SET_OPPORTUNITY_STAGE_IS_SAVING',
+  SET_OPPORTUNITY_OBJECTION_RESOLVED: 'SET_OPPORTUNITY_OBJECTION_RESOLVED'
 }
 
 export function setOpportunity (opportunity) {
@@ -42,5 +43,21 @@ export function saveOpportunityStage (opportunityId, stage) {
     dispatch(setOpportunityStageIsSaving())
     saveStage(opportunityId, stage)
     setTimeout(() => dispatch(setOpportunityStage(stage)), 1000)
+  }
+}
+
+function setOpportunityObjectionResolved (objection) {
+  return {
+    type: ACTION_TYPES.SET_OPPORTUNITY_OBJECTION_RESOLVED,
+    payload: {
+      objection
+    }
+  }
+}
+
+export function toggleOpportunityObjectionResolved (objection, opportunityId) {
+  return dispatch => {
+    writeObjectionResolvedToggle(objection, opportunityId)
+    dispatch(setOpportunityObjectionResolved(objection))
   }
 }
